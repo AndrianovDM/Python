2a45394cf18641a97d114f0323de7f7898b4a75f
Dmitry <andrianovdm1997@mail.ru>
2022-07-14 15:32:11 +0300

    add task 2

--- Homework1/Homework1.py
+++ Homework1/Homework1.py
@@ -3,9 +3,21 @@
 # которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
 
-number_day = int(input("Number day weeks: "))
+# number_day = int(input("Number day weeks: "))
 
-if number_day == 6 or number_day == 7:
-    print(f'{number_day}' + '-> Day off of the week')
-elif 1<= number_day <=5:
-    print(f'{number_day}' + "-> Weekday")
+# if number_day == 6 or number_day == 7:
+#     print(f'{number_day}' + '-> Day off of the week')
+# elif 1<= number_day <=5:
+#     print(f'{number_day}' + "-> Weekday")
+
+
+# Задача №2
+# Напишите программу для. 
+# проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+x, y, z = map(int, input("Введите значение X, Y, Z: ").split())
+if ((not(x or y or z)) == (not x and not y and not z)) == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
+
