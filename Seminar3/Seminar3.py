26e28d71f3520a8c270c23896084ec5a28db6fe8
Dmitry <andrianovdm1997@mail.ru>
2022-07-16 02:50:55 +0300

    add task 2

--- Seminar3/Seminar3.py
+++ Seminar3/Seminar3.py
@@ -2,15 +2,37 @@
 # Реализуйте алгоритм задания случайных чисел 
 # без использования встроенного генератора псевдослучайных чисел.
 
-import time
+# import time
 
-from numpy import _2Tuple
-from sympy import li
+# print(int(str(time.time_ns())[-4] + str(time.time_ns())[-3]))
+# for i in range(10):
+#     time.sleep(0.1)
+#     if i%2 == 0:
+#         print(int(str(time.time_ns())[-4] + str(time.time_ns())[-3]))
+#     else:
+#         print(int(str(time.time_ns())[-4] + str(time.time_ns())[-3]))
 
-print(int(str(time.time_ns())[-4] + str(time.time_ns())[-3]))
-for i in range(10):
-    time.sleep(0.1)
-    if i%2 == 0:
-        print(int(str(time.time_ns())[-4] + str(time.time_ns())[-3]))
-    else:
-        print(int(str(time.time_ns())[-4] + str(time.time_ns())[-3]))
+# Задача №2
+# Задайте список. 
+# Напишите программу, которая определит, присутствует ли в заданном списке строк некое число.
+
+# ПРИМЕР 1
+# def Searchlist(arr, nmber):
+#     for i in arr:
+#         if i == str(nmber):
+#             return 'Yes'
+#     return 'No'
+
+# list1 = ['hello', '12', 'red', '567']
+# print(Searchlist(list1, 12))
+
+# ПРИМЕР 2
+# def Searchlist(arr, nmbers):
+#     if str(nmbers) in arr:
+#         return 'Yes'
+#     return 'No'
+
+# list1 = ['hello', '12', 'red', '567']
+# print(Searchlist(list1, 12))
+
+
