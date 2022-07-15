4838dcb36399fd8bd19f787424433c654203671a
Dmitry <andrianovdm1997@mail.ru>
2022-07-15 03:12:00 +0300

    add task 2

--- Seminar2/Seminar2.py
+++ Seminar2/Seminar2.py
@@ -1,12 +1,38 @@
-# Задача №1
+                                            # Задача №1
 # Число фибоначи
-n = int (input())
-a0 = 0
-a1 = 1
-x = 1
 
-for i in range(n):
-    x = a0 + a1
-    a0 = a1
-    a1 = x
-print(a0)
+# n = int (input())
+# a0 = 0
+# a1 = 1
+# x = 1
+
+# for i in range(n):
+#     x = a0 + a1
+#     a0 = a1
+#     a1 = x
+# print(a0)
+
+                                            # Задача №_2
+# Напишите программу, которая принимает на вход число N 
+# и выдаёт последовательность из N членов.
+# Заполнение случайными числами!
+
+                                    # ПЕРВЫЙ ВАРИАНТ РЕШЕНИЯ
+
+# from random import randint
+# number = int (input('Введите число:'))
+# numbers = []
+# for i in range(number):
+#    num = randint(0,number)
+#    numbers.append(num)
+# print(numbers)
+
+                                    # ВТОРОЙ ВАРИАНТ РЕШЕНИЯ
+# n = int (input('Введите кол-во элементов:'))
+# x = 1
+# for i in range(n):
+#     if n -1 == i:
+#         print(x)
+#     else:
+#         print(x, end = ', ')
+#     x = x *(-3)
