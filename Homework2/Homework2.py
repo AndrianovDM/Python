328df5b99a34b16c093265360121406e6f7cb7d3
Dmitry <andrianovdm1997@mail.ru>
2022-07-15 18:07:11 +0300

    add task 3

--- Homework2/Homework2.py
+++ Homework2/Homework2.py
@@ -12,23 +12,57 @@
 #     sum = sum + (integer % 10)
 #     integer = integer // 10
 
-# while (fractional != 0): # перемножаем числа дробной части
+# while (fractional != 0): 
 #     sum = sum + (fractional % 10)
 #     fractional = fractional // 10
 
-# print("Сумма чисел: ",sum)
+# print("Sum number: ",sum)
 
 # Задача №2
 # Напишите программу, 
 # которая принимает на вход число N и выдает набор произведений чисел от 1 до N. Факториал
 
-number = int(input("Input number: "))
-start = 1
-for i in range(1,number+1):
-    num = i * start
-    start = num
+# number = int(input("Input number: "))
+# start = 1
+# for i in range(1,number+1):
+#     num = i * start
+#     start = num
 
-print(f'{number}! = ', start)
+# print(f'{number}! = ', start)
+
+
+# Задача №3
+# Задана последовательность натуральных чисел, завершающаяся числом 0. 
+# Требуется определить значение второго по величине элемента в этой последовательности, 
+# то есть элемента, который будет наибольшим, если из последовательности удалить наибольший элемент.
+
+number = (list(tuple(input().split(' '))))
+list = []
+for i in number: 
+    list.append(float(i))
+max_number_one =  list[len(list)-1]
+
+for i in list:
+    if i > max_number_one:
+        max_number_one = i
+list.pop(list.index(max_number_one))
+
+max_number_second =  list[len(list)-1]
+
+for j in list:
+    if j > max_number_second:
+        max_number_second = j
+print("Second max number: ", max_number_second)
+
+
+
+
+
+
+
+
+
+
 
 
 
