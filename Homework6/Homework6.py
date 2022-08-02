a996ca12a19f944773b3680caac030eba6e03abb
Dmitry <andrianovdm1997@mail.ru>
2022-08-02 17:17:49 +0300

    add task 2

--- Homework6/Homework6.py
+++ Homework6/Homework6.py
@@ -1,12 +1,23 @@
-# Задача №1
+# Задача №28
 # Напишите такое лямбда выражение transformation, чтобы 
 # transformed_values получился копией values
 
-transformation = lambda val: val
-values = [1, 23, 42, "asdfg"]
-transformed_values =list(map(transformation, values))
-if values == transformed_values:
-    print('OK')
-else:
-    print('FAIL')
+# transformation = lambda val: val
+# values = [1, 23, 42, "asdfg"]
+# transformed_values =list(map(transformation, values))
+# if values == transformed_values:
+#     print('OK')
+# else:
+#     print('FAIL')
 
+
+# Задача №29
+# Самая далекая планета
+
+
+def find_farthest_orbit(list):
+    val = max([(list[i][0]*list[i][1]) for i in range(len(list)) if list[i][0] != list[i][1]])
+    return [list[i] for i in range(len(list)) if list[i][0]*list[i][1] == val][0]
+
+orbits = [(1,3), (2.5, 10), (7,2), (6, 6), (4, 3)]
+print(find_farthest_orbit(orbits))
