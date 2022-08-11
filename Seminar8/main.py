0026ab16c996074c5d427c9a56c2e569c27576dc
Dmitry <andrianovdm1997@mail.ru>
2022-08-11 04:12:21 +0300

    Seminar 8 finish

---
+++ Seminar8/main.py
@@ -0,0 +1,23 @@
+database = {}
+db = {'parents':1, 'children':2}
+
+def reading_file_to_dict(number_file):
+    with open(f'C:\\Users\\Dmitry\\Downloads\\Postgraduate studies\\Homework program\\Python\\Seminar8\\{number_file}.txt', 'r', encoding='utf-8') as file_1:
+        data = [i.split('\n')[0] for i in file_1.readlines()]
+        # print(data)
+        database[number_file] = []
+        for i in data:
+            database[number_file].append(tuple(i.split(';')))
+        print(database)
+
+
+def print_childrens(second_name):
+    id = [i[0] for i in database[db['parents']] if second_name in i][0]
+    deti = [i for i in database[db['children']] if id == i[1]]
+    print(*[' '.join(i[2:4]) + '\n' for i in deti])
+
+reading_file_to_dict(1)
+reading_file_to_dict(2)
+print(database)
+print_childrens('Ivanov')
+# Создать решение для вывода детей по фамилии
